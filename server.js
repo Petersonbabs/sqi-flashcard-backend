350d67aaa0a33593ecd258f940390fb8b8f46e88
petersonbabs <timiebabs@gmail.com>
2025-09-03 11:01:27 +0100

    Added Signup and login endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import connectToDb from "./config/connectToDb.js"
 connectToDb()
 dotenv.config()
@@ -13,6 +14,7 @@ const app = express()
 const PORT = process.env.PORT || 4005
 
 app.use(express.json())
+app.use(cors())
 app.listen(PORT, () => {
     console.log("App is running")
 })
