7967313ec3bd0c0ff748c5e752656152da8b7ef5
petersonbabs <timiebabs@gmail.com>
2025-09-09 10:35:59 +0100

    dev

--- controllers/deckController.js
+++ controllers/deckController.js
@@ -77,3 +77,82 @@ export const getSingleDeck = async (req, res, next) => {
         next(error)
     }
 }
+
+export const deleteDeck = async (req, res, next) => {
+    const { deckId } = req.params
+    if (!deckId) {
+        return res.status(400).json({
+            status: "error",
+            message: "Deck Id is missing"
+        })
+    }
+    try {
+        await deckModel.findByIdAndDelete(deckId)
+        res.status(200).json({
+            status: "success",
+            message: "Deck has been deleted!"
+        })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
+
+
+
+export const generateQuiz = async (req, res, next) => {
+    const { questions } = req.body
+    if (!questions) {
+        return res.status(400).json({
+            status: "error",
+            message: "Questions is missing"
+        })
+    }
+
+    try {
+        const prompt = `You are a professional exam question setter. You are given an array of questions, each question has its own correct answer.
+         Add 3 more incorrect options to the answer in each question in this data: 
+
+        ${JSON.stringify(questions)}.
+
+        Put the correct answer in a random position 
+
+        Do not return any comment or additional text please. just the updated array of question. do not add any special character like *></&^%$#@!;:. The response will be directly render on the frontend and it might break the code.
+
+        Return your response in this format: ${questions.map(ele => {
+            return { question: ele.question, options: [ele.answer, "option2", "option3", "option4"] }
+        })}
+
+        make sure the question are derived from this data: ${JSON.stringify(questions)}
+
+        > Thank you
+        `
+
+        // const prompt = `Return the question back ${JSON.stringify(questions)}`
+
+        const response = await fetch(`${process.env.GEMINI_URL}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-goog-api-key": process.env.GEMINI_API_KEY
+            },
+            body: JSON.stringify({
+                contents: [
+                    {
+                        parts: [
+                            {
+                                text: prompt
+                            }
+                        ]
+                    }
+                ]
+            })
+        })
+        const data = await response.json()
+        res.json({ quiz: data.candidates[0].content.parts[0].text.replaceAll("\n", " ") })
+
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
--- routers/deckRouter.js
+++ routers/deckRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
-import { addNewDeckHandler, getMyDecksHandler, getSingleDeck } from "../controllers/deckController.js";
+import { addNewDeckHandler, deleteDeck, generateQuiz, getMyDecksHandler, getSingleDeck } from "../controllers/deckController.js";
 const deckRouter = Router()
 
 deckRouter.post("/", isLoggedIn, addNewDeckHandler)
 deckRouter.get("/", isLoggedIn, getMyDecksHandler)
 deckRouter.get("/:deckId", isLoggedIn, getSingleDeck)
+deckRouter.post("/generate-quiz", isLoggedIn, generateQuiz)
+deckRouter.delete("/:deckId", isLoggedIn, deleteDeck)
 
 export default deckRouter
